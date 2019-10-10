2ae9af9b4c4b63dbe4346bdaaf41a909e069d40e
等天黑 <wx_34e4dd2b91054771afccf8e8f8cddae0@git.code.tencent.com>
2019-10-10 10:21:49 +0800

    commit

---
+++ Christ3D/Chris3D.UI.Web/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Chris3D.UI.Web.Models;
+
+namespace Chris3D.UI.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
